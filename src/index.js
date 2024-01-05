3bfac43fb9632d2693a4e3e936095517f9437176
E-Korsakova <evild3vilcat@gmail.com>
2024-01-05 10:27:27 +0300

    исправлены ошибки

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
 
+import reportWebVitals from './reportWebVitals';
 import App from './components/App';
 import './index.css';
 
-const rootEl = document.getElementById('root')
+const rootEl = document.getElementById('root');
 const root = createRoot(rootEl);
 root.render(<App />);
 
